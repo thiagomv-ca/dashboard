b0a014739136a3b3a3214f6acb6e0bd989a408b5
Thiago Meneses Vieira <thiagomenesesvieira@Thiago.local>
2022-08-20 01:11:51 -0700

    fix: remove problem in sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -6,56 +6,56 @@ export function SideBar() {
       <Stack spacing="12" align="flex-start">
         <Box>
           <Stack spacing="4" mt="8" align="stretch">
-          <Link display="flex" align="center">
+          <Link display="flex">
             <Icon as={RiDashboardLine} fontSize="20" />
             <Text ml="4" fontWeight="medium">
               Dashboard
             </Text>
           </Link>
 
-          <Link display="flex" align="center">
+          <Link display="flex">
             <Icon as={RiPagesFill} fontSize="20" />
             <Text ml="4" fontWeight="medium">
               Data Entry
             </Text>
           </Link>
-          <Link display="flex" align="center">
+          <Link display="flex" >
             <Icon as={RiPagesFill} fontSize="20" />
             <Text ml="4" fontWeight="medium">
               Documents
             </Text>
           </Link>
-          <Link display="flex" align="center">
+          <Link display="flex" >
             <Icon as={RiPagesFill} fontSize="20" />
             <Text ml="4" fontWeight="medium">
               Mechanical
             </Text>
           </Link>
-          <Link display="flex" align="center">
+          <Link display="flex" >
             <Icon as={RiPagesFill} fontSize="20" />
             <Text ml="4" fontWeight="medium">
               Invoices
             </Text>
           </Link>
-          <Link display="flex" align="center">
+          <Link display="flex" >
             <Icon as={RiPagesFill} fontSize="20" />
             <Text ml="4" fontWeight="medium">
               Administrations
             </Text>
           </Link>
-          <Link display="flex" align="center">
+          <Link display="flex" >
             <Icon as={RiPagesFill} fontSize="20" />
             <Text ml="4" fontWeight="medium">
               Review
             </Text>
           </Link>
-          <Link display="flex" align="center">
+          <Link display="flex" >
             <Icon as={RiPagesFill} fontSize="20" />
             <Text ml="4" fontWeight="medium">
               Management
             </Text>
           </Link>
-          <Link display="flex" align="center">
+          <Link display="flex" >
             <Icon as={RiPagesFill} fontSize="20" />
             <Text ml="4" fontWeight="medium">
               Reports
